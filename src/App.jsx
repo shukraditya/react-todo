d0f2516464846798a544d94f235714f2279c9f77
shukraditya <shukra12bose@gmail.com>
2024-12-30 16:04:21 +0530

    split it into components, learnt what props are

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 
 import {useState} from "react";
 import "./styles.css"
+import {NewTodoForm} from "./NewTodoForm.jsx";
+import {TodoList} from "./TodoList.jsx";
 
 
 
 
 
 export default function App(){
-  const [newItem,setNewItem] = useState("")
+
   const [todos, setTodos] = useState([])
 
-  function handleSubmit(e){
-    e.preventDefault()
-
+  function addTodo(title){
     setTodos(currentTodos=>{
       return[
         ...currentTodos,
-        {id:crypto.randomUUID(),title:newItem,completed:false},
+        {id:crypto.randomUUID(),title,completed:false},
       ]
     })
-    setNewItem("")
   }
-
-function toggleTodo(id,completed){
+  function toggleTodo(id,completed){
     setTodos(currentTodos=>{
       return currentTodos.map(todo=>{
         if(todo.id===id){
@@ -33,40 +31,18 @@ function toggleTodo(id,completed){
     })
   }
 
-function deleteTodo(id){
+  function deleteTodo(id){
     setTodos(currentTodos=>{
       return currentTodos.filter(todo=>todo.id!==id)
     })
   }
 
-  return(
-    <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input
-            value={newItem}
-            onChange={e=>setNewItem(e.target.value)}
-            type="text" id="item"/>
-        </div>
-        <button className="btn">Add</button>
-      </form>
-      <h1 className="header">Todo List</h1>
-      <ul className="list">
-        {todos.length===0&&"No Todos"}
-        {todos.map(todo=>{
-          return(
-            <li key={todo.id}>
-              <label>
-                <input type="checkbox" checked={todo.completed}
-                onChange={e =>toggleTodo(todo.id,e.target.checked) }
-                /> {todo.title}
-              </label>
-              <button
-                onClick={()=>deleteTodo(todo.id)}
-                className="btn btn-danger">Delete</button>
-            </li>
-          )})}
-      </ul>
-    </>);
-}
+
+    return(
+      <>
+        <NewTodoForm onSubmit={addTodo}/>
+        <h1 className="header">Todo List</h1>
+        <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+      </>
+    );
+  }
