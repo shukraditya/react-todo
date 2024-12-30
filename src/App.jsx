070c46a6122f92887c6f3c46405f4cada0d2ae75
shukraditya <shukra12bose@gmail.com>
2024-12-30 14:09:23 +0530

    delete and checking works

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import "./styles.css"
 
 
 export default function App(){
-  const [newItem,setNewItem] = useState("Item")
+  const [newItem,setNewItem] = useState("")
   const [todos, setTodos] = useState([])
 
   function handleSubmit(e){
@@ -19,8 +19,25 @@ export default function App(){
         {id:crypto.randomUUID(),title:newItem,completed:false},
       ]
     })
+    setNewItem("")
   }
 
+function toggleTodo(id,completed){
+    setTodos(currentTodos=>{
+      return currentTodos.map(todo=>{
+        if(todo.id===id){
+          return{...todo,completed}
+        }
+        return todo
+      })
+    })
+  }
+
+function deleteTodo(id){
+    setTodos(currentTodos=>{
+      return currentTodos.filter(todo=>todo.id!==id)
+    })
+  }
 
   return(
     <>
@@ -36,13 +53,18 @@ export default function App(){
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length===0&&"No Todos"}
         {todos.map(todo=>{
           return(
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} /> {todo.title}
+                <input type="checkbox" checked={todo.completed}
+                onChange={e =>toggleTodo(todo.id,e.target.checked) }
+                /> {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button
+                onClick={()=>deleteTodo(todo.id)}
+                className="btn btn-danger">Delete</button>
             </li>
           )})}
       </ul>
